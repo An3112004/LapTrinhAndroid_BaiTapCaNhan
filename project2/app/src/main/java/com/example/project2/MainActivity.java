37ef33c81d391656175901791f5e029ff52171c6
An3112004 <An3112k4@gmail.com>
2025-05-09 15:20:32 +0700

    Hoàn thành BT2

--- project2/app/src/main/java/com/example/project2/MainActivity.java
+++ project2/app/src/main/java/com/example/project2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.project2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,51 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toast.makeText(this,"Cr424-onCreate()" , Toast.LENGTH_SHORT).show();
+        btnCall.findViewById(R.id.button);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"Cr424-onRestart()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"Cr424-onStart()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"Cr424-onStop()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Cr424-onDestroy()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"Cr424-onPause()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Toast.makeText(this,"Cr424-onResume()",Toast.LENGTH_SHORT).show();
     }
 }
