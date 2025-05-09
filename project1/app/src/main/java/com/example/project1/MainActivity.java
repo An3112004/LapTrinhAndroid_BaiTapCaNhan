4e03de0c67c98be081de805523fa29b6d00bde5e
An3112004 <An3112k4@gmail.com>
2025-05-09 14:54:02 +0700

    Hoàn thành BT1

--- project1/app/src/main/java/com/example/project1/MainActivity.java
+++ project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.project1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtA , edtB , edtKQ;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        edtA = findViewById(R.id.editTextText);
+        edtB = findViewById(R.id.editTextText2);
+        edtKQ = findViewById(R.id.editTextText3);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+
+                int c = a + b;
+                edtKQ.setText(c + "");
+            }
         });
     }
 }
