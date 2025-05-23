b5251d2480495daf05d2ecc571ae90f5fe24817b
An3112004 <An3112k4@gmail.com>
2025-05-23 10:23:49 +0700

    Bài 12 + Bài 13

---
+++ project13_2_GridView/app/src/main/java/com/example/project13_2_gridview/DetailActivity.java
@@ -0,0 +1,21 @@
+package com.example.project13_2_gridview;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        txtResult = findViewById(R.id.txtResult);
+        String phoneName = getIntent().getStringExtra("phone_name");
+        txtResult.setText("Bạn đã chọn: " + phoneName);
+    }
+}
